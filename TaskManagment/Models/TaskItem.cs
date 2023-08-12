3575f88c45399860b041e9b86c58094fd32c09d0
KarolinaAugustyniak <kar.aug00@gmail.com>
2023-08-12 02:22:35 +0200

    Add taskItem model

---
+++ TaskManagment/Models/TaskItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManagment.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
