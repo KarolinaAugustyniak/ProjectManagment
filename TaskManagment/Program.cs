3fdec8834bae56a99402be6891bb94b761146c3f
KarolinaAugustyniak <kar.aug00@gmail.com>
2023-08-17 22:24:44 +0200

    Add swagger

--- TaskManagment/Program.cs
+++ TaskManagment/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -11,7 +15,15 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
