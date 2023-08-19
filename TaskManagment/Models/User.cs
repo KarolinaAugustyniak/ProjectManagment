5f85bf68885cf3d5d1ab1de2231dcb29925023fd
KarolinaAugustyniak <kar.aug00@gmail.com>
2023-08-19 23:51:21 +0200

    Add user model

---
+++ TaskManagment/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace TaskManagment.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Username { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string PasswordHash { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Position { get; set; } 
+
+    }
+}
