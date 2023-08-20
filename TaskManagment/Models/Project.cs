aa59298530b85d53d7a6b7e9b8f60e1ba60bd07a
KarolinaAugustyniak <kar.aug00@gmail.com>
2023-08-20 22:15:04 +0200

    Add project model

---
+++ TaskManagment/Models/Project.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManagment.Models
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(250)")]
+        public string ProjectName { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("User")]
+        public int CreatedBy { get; set; }
+
+        [ForeignKey("Organization")]
+        public int? OrganizationId { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Organization Organization { get; set; }
+    }
+}
